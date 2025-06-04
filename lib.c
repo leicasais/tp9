fa54a6c120a455c8ac3ec57b548c521a69e39fd8
leicasais <lcasais@itba.edu.ar>
2025-06-04 09:32:01 -0300

    Agrego struct y union, pos vista de codigo de sol

--- lib.c
+++ lib.c
@@ -1,7 +1,7 @@
 #include "lib.h"
 
 void bitset(void **pp_puerto,int num_bit, int size){//Admite puertos con 1 byte o 2 bytes
-    if (size==1){
+    if (size==1){//Mucho mas facil si uso | en vez de & y se puede hacer en un #define
         uint8_t *p=(uint8_t*)*pp_puerto;
         *p+=OFF_ON(*p,num_bit);
     }
--- lib.h
+++ lib.h
@@ -9,6 +9,20 @@
     #define BIT_1 1
     #define OFF_ON(p_bit,num_bit) ((((p_bit)>>(num_bit)) & BIT_1)?0:(BIT_1<<(num_bit)))//Si el Bit esta apagado en el lugar num_bit, le suma el numero equivalente a ese lugar prendido
     
+    
+    //var compratidas: 
+    typdef struct {
+    uint8_t b;//Guarda primero, en la menor direccion b (independientemente del endinnes)
+    uint8_t a;
+    }registro_t;
+
+    typedef union{
+        registro_t registro;
+        uint16_t d;
+    }puerto_t;
+
+
+
     //Prototipos de fun:
     void bitset(void **p_puerto,int num_bit, int size);
 
